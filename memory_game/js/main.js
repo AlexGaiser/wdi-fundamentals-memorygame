9c0aa6e3d912b491e0141dcdc3cf672f233f1977
Alexander Gaiser <alexandergaiser@ool-ad02056c.dyn.optonline.net>
2018-12-03 08:57:02 -0500

    redo of part 7

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,32 @@
 
-console.log("up and running!");
 
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var flipcard = function(cardid){
+     var cardOne = "queen";
+
+
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+
+
+cardsInPlay.push(cardOne)
+cardsInPlay.push(cardTwo)
+
+
+
+console.log()
+
+
+
 
 console.log("user flipped " + cardOne);
 console.log("user flipped " + cardThree);
+
+console.log(cardOne !==cardThree)
+}
+
+var cards =["queen", "queen", "king", "king"]
+
+var cardsInPlay = []
+
